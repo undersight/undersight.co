a82c41e5134164802d6edee481a1bfcde6ba1b78
Eduardo Nunes <ed@eduardonunes.me>
2018-03-27 23:03:21 +0100

    Add metadata

--- src/html.js
+++ src/html.js
@@ -29,6 +29,7 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
           />
+
           {this.props.headComponents}
           {css}
         </head>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -39,10 +39,36 @@ class TemplateWrapper extends React.Component {
           <Helmet
             title="Undersight.co"
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'name', content: 'Undersight.co' },
+              { name: 'description', content: 'Undersight.co is the digital playground of Eduardo Nunes. Full-time graphic designer, part-time noise maker. Head of Design at Stockholm-based Kollegorna.' },
+              { name: 'keywords', content: 'undersight, design, eduardo nunes, Web, development, frontend, graphic' },
+              { name: 'image', content: "/images/og.png" },
             ]}
-          />
+          >
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#ff4d00" />
+            <meta name="msapplication-TileColor" content="#fff9aa" />
+            <meta name="theme-color" content="#fff9aa" />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="Undersight.co" />
+            <meta property="og:title" content="Undersight.co" />
+            <meta property="og:description" content="Undersight.co is the digital playground of Eduardo Nunes. Full-time graphic designer, part-time noise maker. Head of Design at Stockholm-based Kollegorna." />
+            <meta property="og:image" content="/images/og.png" />
+            <meta property="og:url" content={"https://www.undersight.co" + this.props.location.pathname} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content="Undersight.co" />
+            <meta name="twitter:description" content="Undersight.co is the digital playground of Eduardo Nunes. Full-time graphic designer, part-time noise maker. Head of Design at Stockholm-based Kollegorna." />
+            <meta name="twitter:image:src" content="/images/og.png" />
+            <meta name="twitter:site" content="@emmnunes" />
+            <meta name="twitter:creator" content="@emmnunes" />
+
+          </Helmet>
 
           <Header headerSize={inRegexArray(this.props.location.pathname, largeHeaderPages) ? 'large' : 'small'} />
 
