dd3cb0b1af0e962d239cb69cdc05a3d3221343ad
Eduardo Nunes <ed@eduardonunes.me>
2018-03-27 23:12:04 +0100

    Add basic 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import styled, { ThemeProvider } from "styled-components"
+import { rem, hideVisually } from "polished"
+
+const Error = styled.section`
+  width: 100%;
+  margin-top: ${rem(80)};
+  padding: ${rem(50)} ${rem(30)};
+  text-align: center;
+
+  h1 {
+    font-family: ${props => props.theme.ffPrimary};
+    font-size: ${rem(24)};
+    line-height: 1;
+    font-weight: 700;
+    margin: 0;
+
+    @media ${props => props.theme.smallUp} {
+      font-size: ${rem(36)};
+    }
+
+    @media ${props => props.theme.mediumUp} {
+      font-size: 4vw;
+    }
+
+    @media ${props => props.theme.largeUp} {
+      font-size: 3vw;
+    }
+    
+    @media ${props => props.theme.xxlargeUp} {
+      font-size: ${rem(72)};
+    }
+  }
+`
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Error>
+      <h1>😔<br />Page not found...</h1>
+    </Error>
   </div>
 )
 
