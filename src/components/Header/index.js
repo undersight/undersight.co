33f4d76e0e30412bb900c9e777d00af2d764ee64
Eduardo Nunes <ed@eduardonunes.me>
2020-12-19 23:28:47 +0000

    Replace Kollegorna with Odd Camp

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -193,7 +193,7 @@ class Header extends React.Component {
           <SiteTitle><strong>Undersight.co</strong> is the digital playground of Eduardo Nunes.</SiteTitle>
           <SiteByline>
             Full-time graphic designer, part-time noise maker. <br />
-            Head of Design at Stockholm-based <a href="https://www.kollegorna.se/en/" target="_blank" title="Kollegorna's Website" rel="noopener noreferrer">Kollegorna</a>.
+            Head of Design at Stockholm-based <a href="https://www.odd.camp" target="_blank" title="Odd Camp's Website" rel="noopener noreferrer">Odd Camp</a>.
           </SiteByline>
         </MainHeader>
       )
